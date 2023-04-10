f041ded5aeeb3b2c6cf435a7c0ca94eaf2df0922
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-04-10 05:13:17 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,30 @@
-pub mod encryption {
-    pub mod caesar;
-    pub mod vigenere;
-    pub mod aes;
-    pub mod des;
-    pub mod rsa;
+// Import necessary modules
+pub mod encryption;
+pub mod utils;
+
+// Declare the crate as a workspace
+// This is required to enable the use of features across multiple crates in the workspace
+// For example, if the caesar feature is enabled in SadieFish, it will also be enabled in any other
+// crate in the workspace that depends on SadieFish.
+// This is useful for testing and for avoiding version mismatches between crates.
+#[cfg(not(test))] // Only include this section in non-test builds
+#[cfg(feature = "workspace")]
+pub mod workspace {
+    pub use SadieFish_encryption as encryption;
+    pub use SadieFish_utils as utils;
 }
 
-pub mod utils {
-    pub mod base64;
-    pub mod hex;
-    pub mod bit_ops;
-    pub mod rand;
-}
+// Declare the encryption module as the default feature
+#[cfg(not(test))] // Only include this section in non-test builds
+#[cfg(feature = "default")]
+pub use SadieFish_encryption::*;
 
-#[cfg(test)]
-mod tests {
-    mod utils_tests;
-    mod encryption_tests;
-}
+// Declare features for the various encryption algorithms and utilities
+#[cfg(not(test))] // Only include this section in non-test builds
+#[cfg(any(feature = "caesar", feature = "vigenere", feature = "aes", feature = "des", feature = "rsa"))]
+#[cfg(not(feature = "default"))]
+pub use SadieFish_encryption::*;
+
+#[cfg(not(test))] // Only include this section in non-test builds
+#[cfg(any(feature = "base64", feature = "hex", feature = "bit_ops"))]
+pub use SadieFish_utils::*;
